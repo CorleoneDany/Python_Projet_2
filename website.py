67dcec30e7404ed5669108300d7210d4b97f9d27
CorleoneDany <corleonedany@live.fr>
2020-11-05 18:58:37 +0100

    ajout d'un message de confirmation dans l'objet website, site renommé en website pour cause de conflit avec python de base, la méthode collect de la classe collector est désormais fonctionnelle

---
+++ website.py
@@ -0,0 +1,20 @@
+import subprocess
+import sys
+import requests
+
+class Website:
+    def __init__(self):
+        self.page_number = "1"
+        self.url = "https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"  
+        self.content = ""
+
+    def requestWebsite(self):
+        print("Tentative de connection à l'url books.toscrape.com")
+        response = requests.get(self.url)
+        if response.ok:
+            print("Connection ok")
+            print("La requête à retourné des informations : ")
+            self.content = response.text
+        else:
+            print("La requête à retourné une erreur : ")
+            print(requests.status_codes)
